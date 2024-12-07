0832adb7001f2fe84d36f367a9a3ffb981eb0d98
Trong <spektralfrogadier@gmail.com>
2024-12-06 16:21:08 -0800

    rework modes

---
+++ src/modes/extra/CupHead.cpp
@@ -0,0 +1,52 @@
+#include "modes/extra/CupHead.hpp"
+
+#define ANALOG_STICK_MIN 1
+#define ANALOG_STICK_NEUTRAL 128
+#define ANALOG_STICK_MAX 255
+
+CupHead::CupHead(socd::SocdType socd_type) {
+    _socd_pair_count = 4;
+    _socd_pairs = new socd::SocdPair[_socd_pair_count]{
+        socd::SocdPair{&InputState::left,    &InputState::right,   socd_type},
+        socd::SocdPair{ &InputState::down,   &InputState::mod_x,   socd_type},
+        socd::SocdPair{ &InputState::c_left, &InputState::c_right, socd_type},
+        socd::SocdPair{ &InputState::c_down, &InputState::c_up,    socd_type},
+    };
+}
+
+void CupHead::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) {
+    // CupHead is played via the DPAD
+    outputs.dpadLeft = inputs.left;
+    outputs.dpadRight = inputs.right;
+    outputs.dpadDown = inputs.down;
+    outputs.dpadUp = inputs.mod_x;
+
+    outputs.y = inputs.b; // Attack
+    outputs.a = inputs.a; // Super Attack (Cards)
+    outputs.b = inputs.x; // Jump
+    outputs.x = inputs.z; // Dash
+
+    outputs.buttonL = inputs.r; // Change Weapon
+
+    outputs.buttonR = inputs.l; // Stationary
+
+    outputs.select = inputs.y;
+    outputs.start = inputs.start;
+}
+
+void CupHead::UpdateAnalogOutputs(InputState &inputs, OutputState &outputs) {
+    UpdateDirections(
+        inputs.left,
+        inputs.right,
+        inputs.down,
+        inputs.mod_x,
+        inputs.c_left,
+        inputs.c_right,
+        inputs.c_down,
+        inputs.c_up,
+        ANALOG_STICK_MIN,
+        ANALOG_STICK_NEUTRAL,
+        ANALOG_STICK_MAX,
+        outputs
+    );
+}
--- src/modes/extra/Taiko.cpp
+++ src/modes/extra/Taiko.cpp
@@ -1,32 +1,36 @@
-#include "modes/MK8D.hpp"
+#include "modes/extra/Taiko.hpp"
 
 // Nintendo Switch Analong Angles; DONT CHANGE
 #define ANALOG_STICK_MIN 28
 #define ANALOG_STICK_NEUTRAL 128
 #define ANALOG_STICK_MAX 228
 
-/*
- * soft drift angle func?
- * soft drift = modY: max left + right
+/* 
+ * Taiko No Tatsujin Rhythm Festival
+ * Y, B, Ddown, Dright = Don/Red
+ * A, X, L, R Dleft, Dup = Ka/Blue 
 */
 
-MK8D::MK8D(socd::SocdType socd_type) {
+TAIKO::TAIKO(socd::SocdType socd_type) {
     _socd_pair_count = 4;
     _socd_pairs = new socd::SocdPair[_socd_pair_count]{
-        // base left stick inputs
+        // Base Joystick Inputs
         socd::SocdPair{ &InputState::left,   &InputState::right, socd_type },
         socd::SocdPair{ &InputState::down,   &InputState::up, socd_type },
-        // c_up & c_down act as accelerate/break
+        // CStick = Right Stick
         socd::SocdPair{ &InputState::c_left, &InputState::c_right, socd_type },
         socd::SocdPair{ &InputState::c_down, &InputState::c_up, socd_type }
     };
 }
 
-void MK8D::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) {
-    // change the layout
-    outputs.a = inputs.b;
-    outputs.b = inputs.a;
-    outputs.x = inputs.l;
+void TAIKO::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) {
+    // The two mainrow buttons both act as Don
+    outputs.b = inputs.b;
+    outputs.x = inputs.y;
+
+    // Next two act as Ka (figure out how to change UP)
+    outputs.a = inputs.z;
+    //outputs. = inputs.l;
     //outputs.y = inputs.c_down;
     outputs.buttonL = inputs.z;
     outputs.buttonR = inputs.x;
@@ -39,23 +43,17 @@ void MK8D::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) {
     outputs.dpadLeft = inputs.r;
     outputs.dpadRight = inputs.midshield;
 
+    // IF modY is held ZL & ZR will act as L & R
     if (inputs.mod_y) {
-        // ZL & ZR will act as L & R
         outputs.triggerLDigital = inputs.z;
         outputs.triggerRDigital = inputs.x;
-
-        // A & B swap positions
-        outputs.a = inputs.a;
-        outputs.b = inputs.b;
     } else if (inputs.mod_y && inputs.c_down) {
-        outputs.home = inputs.start;
+        outputs.home = inputs.start;  
     }
 }
 
-void MK8D::UpdateAnalogOutputs(InputState &inputs, OutputState &outputs) {
+void TAIKO::UpdateAnalogOutputs(InputState &inputs, OutputState &outputs) {
     bool up = inputs.mod_x;
-    //bool mod_x = inputs.up;
-
     UpdateDirections(
         inputs.left,
         inputs.right,
@@ -70,18 +68,4 @@ void MK8D::UpdateAnalogOutputs(InputState &inputs, OutputState &outputs) {
         ANALOG_STICK_MAX,
         outputs
     );
-
-    /*if (mod_x) {*/
-    /*    outputs.leftStickY = ANALOG_STICK_MAX;*/
-    /*} else if (inputs.down) {*/
-    /*    outputs.leftStickY = ANALOG_STICK_MIN;*/
-    /*} else {*/
-    /*    outputs.leftStickY = ANALOG_STICK_NEUTRAL;*/
-    /*}*/
-
-    // Nunchuk Support
-    if (inputs.nunchuk_connected) {
-        outputs.leftStickX = inputs.nunchuk_x;
-        outputs.leftStickY = inputs.nunchuk_y;
-    }
 }
